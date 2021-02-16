cd2a4424c66ddbd463854c76a3f068ddc7fdfd98
Rich Worsley <rworsl01@dcs.bbk.ac.uk>
2021-02-16 23:35:56 +0000

    reads in mealplan text file and sorts into categories

--- MealPlanner/MainWindow.xaml.cs
+++ MealPlanner/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MealPlanner
 {
@@ -20,9 +11,55 @@ namespace MealPlanner
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string[]> redMeat = new List<string[]> { };
+        List<string[]> whiteMeat = new List<string[]> { };
+        List<string[]> veg = new List<string[]> { };
+        List<string[]> fish = new List<string[]> { };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void readMealsList()
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\RichW\Desktop\Projects\MealPlanner\MealPlanner\bin\mealplan.txt");
+            classifyFood(file);
+        }
+
+        private void classifyFood(StreamReader file)
+        {
+            string line = "";
+            while ((line = file.ReadLine()) != null)
+            {
+                var lineTabs = line.Split('\t');
+                if(lineTabs[9] == "Red Meat")
+                {
+                    redMeat.Add(lineTabs);
+                }
+                else if (lineTabs[9] == "White Meat")
+                {
+                    whiteMeat.Add(lineTabs);
+                }
+                else if (lineTabs[9] == "Fish")
+                {
+                    fish.Add(lineTabs);
+                }
+                else if (lineTabs[9] == "Veg")
+                {
+                    veg.Add(lineTabs);
+                }
+            }
+        }
+
+        /// <summary>
+        /// When "run" button is pressed by the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            readMealsList();
         }
     }
 }
